080ba3dde759e308a35034128cbfd545fabc1ec3
jh.park <hongzzi.dev@gmail.com>
2019-02-01 09:36:22 +0900

    DP 2302 극장좌석 +수정
    백준 런타임 오류로 코드 재구성함, 점화식은 구했지만 코드를 효율적으로 짜지 못했음.
    점화식 dp[i]= dp[i-1] + dp[i-2]
    
    2019.01.30

---
+++ Baekjoon/src/DP1463.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+public class DP1463 {
+
+    public static void main(String[] args) throws Exception {
+
+        //BufferedReader 객체 생성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int sits = Integer.parseInt(br.readLine());
+        int vip = Integer.parseInt(br.readLine());
+
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
--- Baekjoon/src/DP2302.java
+++ Baekjoon/src/DP2302.java
@@ -10,46 +10,31 @@ public class DP2302 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int sits = Integer.parseInt(br.readLine());
-        int vn = Integer.parseInt(br.readLine());
-        int[] vip = new int[vn];
-        int[] part = new int[vn+1];
+        int[] dp = new int[sits+1];
+        int pre, answer = 1, cur = 0;
 
 
-        vip[0] = Integer.parseInt(br.readLine());
-        part[0] = vip[0]-1;
+        fib(dp,sits);
 
-        int maximum = part[0];
+        int vipn = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i<vn; i++) {
-            vip[i] = Integer.parseInt(br.readLine());
-            part[i] = (vip[i]-vip[i-1])-1;
-            maximum = Math.max(maximum,part[i]);
+        for (int i=0; i<vipn; i++) {
+            pre = Integer.parseInt(br.readLine());
+            answer *= dp[pre-cur-1];
+            cur = pre;
         }
-        part[vn] = sits-vip[vn-1];
-        maximum = Math.max(maximum,part[vn]);
+        answer *= dp[sits-cur];
 
-
-        int[] dp = new int[maximum+1];
-        dp[0]=1;
-        dp[1]=1;
-        dp[2]=2;
-
-
-        if(maximum>2) {
-            for(int j = 3; j<=maximum; j++) {
-                dp[j] = dp[j-1]+dp[j-2];
-            }
-        }
-
-        int result = 1;
-        for (int k=0; k<part.length; k++){
-            result *= dp[part[k]];
-        }
-
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
 
     }
+
+    private static void fib(int []dp, int n ) {
+        dp[0]=1; dp[1]=1;
+        for(int i=2; i<=n; i++) dp[i] = dp[i-1]+dp[i-2];
+    }
+
 
 }
