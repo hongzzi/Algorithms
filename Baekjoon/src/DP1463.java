f1c7979206806aad6b96789b915d51d989a3c942
jh.park <hongzzi.dev@gmail.com>
2019-02-01 14:47:22 +0900

    DP 1463 1로 만들기
    점화식 dp[i] = dp[i-1] + 1 or dp[i] = dp[n/2] + 1 or dp[i] = dp[n/3] + 1
    
    2019.02.01

--- Baekjoon/src/DP1463.java
+++ Baekjoon/src/DP1463.java
@@ -9,10 +9,17 @@ public class DP1463 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int sits = Integer.parseInt(br.readLine());
-        int vip = Integer.parseInt(br.readLine());
+        int num= Integer.parseInt(br.readLine());
+        int[] dp = new int[num+1];
+        dp[0] = dp[1] = 0;
 
+        for(int i=2; i<=num; i++) {
+            dp[i] = dp[i-1] +1;
+            if(i%2==0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+            if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+        }
 
+        bw.write(String.valueOf(dp[num]));
         bw.flush();
         bw.close();
 
